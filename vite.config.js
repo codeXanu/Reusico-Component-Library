8d82d1b52a9537ec109e9a74453e793d7798ace8
Anuj Maurya <anuj24122001@gmail.com>
2025-04-20 04:02:19 +0530

    last one

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,36 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+const isDemoBuild = process.env.BUILD_TARGET === 'demo';
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/index.js'),
-      name: 'ReusicoComponentLibrary',
-      fileName: (format) =>
-        format === 'es'
-          ? 'reusico-component-library.esm.js'
-          : format === 'cjs'
-          ? 'reusico-component-library.cjs.js'
-          : `reusico-component-library.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+  build: isDemoBuild
+    ? {
+        outDir: 'demo-dist',
+        emptyOutDir: true,
+      }
+    : {
+        lib: {
+          entry: path.resolve(__dirname, 'src/components/index.js'),
+          name: 'ReusicoComponentLibrary',
+          fileName: (format) =>
+            format === 'es'
+              ? 'reusico-component-library.esm.js'
+              : format === 'cjs'
+              ? 'reusico-component-library.cjs.js'
+              : `reusico-component-library.${format}.js`,
         },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
+        },
+        outDir: 'dist',
+        emptyOutDir: true,
       },
-    },
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 });
-
-
-
-
