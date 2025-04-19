6195691800f45ca47947954015287b6c29be0904
Anuj Maurya <anuj24122001@gmail.com>
2025-04-20 02:24:35 +0530

    Fix: deploymeny on vercel

--- vite.config.js
+++ vite.config.js
@@ -2,32 +2,40 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+const isLibBuild = process.env.BUILD === 'lib';
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Output directory for static files
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/index.js'),
-      name: 'ReusicoComponentLibrary',
-      fileName: (format) =>
-        format === 'es'
-          ? 'reusico-component-library.esm.js'
-          : format === 'cjs'
-          ? 'reusico-component-library.cjs.js'
-          : `reusico-component-library.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+  build: isLibBuild
+    ? {
+        lib: {
+          entry: path.resolve(__dirname, 'src/components/index.js'),
+          name: 'ReusicoComponentLibrary',
+          fileName: (format) =>
+            format === 'es'
+              ? 'reusico-component-library.esm.js'
+              : format === 'cjs'
+              ? 'reusico-component-library.cjs.js'
+              : `reusico-component-library.${format}.js`,
         },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
+        },
+        outDir: 'dist-lib', // for npm
+        emptyOutDir: true,
+      }
+    : {
+        outDir: 'dist', // for Vercel
+        emptyOutDir: true,
       },
-    },
-  },
-  root: './', // Ensure root is the root directory (including index.html)
 });
+
 
 
 
